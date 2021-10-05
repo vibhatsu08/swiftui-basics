2e14fe707c4f32d98cf205023dfe28d98160abd7
Vedant Mistry <37664832+vibhatsu08@users.noreply.github.com>
2021-10-06 00:08:32 +0530

    integrating sprite kit with sprite view

---
+++ integrateSpriteView/integrateSpriteView/integrateSpriteViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  integrateSpriteViewApp.swift
+//  integrateSpriteView
+//
+//  Created by Vedant Mistry on 05/10/21.
+//
+
+import SwiftUI
+
+@main
+struct integrateSpriteViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
